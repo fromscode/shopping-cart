bc969d1608f16971aa4b17316bfa1dbe75aaa29e
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-01-14 06:33:14 +0000

    Style shop page (wip)

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,13 +1,37 @@
 import { useOutletContext } from "react-router";
 
 export default function Shop() {
-	const [data, loading, error] = useOutletContext();
+  const [data, loading, error] = useOutletContext();
 
-	if (loading) return <div>Loading</div>;
+  if (loading) return <div>Loading</div>;
 
-	if (error) return <div>Error</div>;
+  if (error) return <div>Error</div>;
 
-	console.log(data);
+  console.log(data);
 
-	return data.map((item) => <div key={item.id}>{item.title}</div>);
+  return (
+    <div className="shop">
+      {data.map((item) => (
+        <Card key={item.id} item={item}></Card>
+      ))}
+    </div>
+  );
+}
+
+function Card({ item }) {
+  return (
+    <div className="card">
+      <img
+        src={item.image}
+        alt={`Image of ${item.title}`}
+        height={150}
+        width={"auto"}
+      />
+      <h3>{item.title}</h3>
+      <p>
+        Ratings: {item.rating.rate} ({item.rating.count})
+      </p>
+      <button type="button">Add to cart</button>
+    </div>
+  );
 }
