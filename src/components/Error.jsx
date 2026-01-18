c421d293e2feaf578d890a27bc64b3934cfd0add
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-01-18 07:04:51 +0000

    Style default error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,10 @@
 export default function Error() {
   return (
     <>
-      <div>This is error page</div>
+      <div className="error">
+        Oops! Something went wrong
+        <p>(this is probably your fault)</p>
+      </div>
     </>
   );
 }
